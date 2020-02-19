817a261cf5dece4387c60b7dcb34c8ca1f6220ab
VayFull <40316778+VayFull@users.noreply.github.com>
2020-02-20 00:41:50 +0300

    push

---
+++ BotExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace BotExample
+{
+    class Program
+    {
+        private static ITelegramBotClient _botClient;
+
+        static void Main(string[] args)
+        {
+            _botClient = new TelegramBotClient("Ваш токен")
+                {Timeout = TimeSpan.FromSeconds(10)};
+
+            _botClient.OnMessage += Bot_OnMessage;
+            _botClient.StartReceiving();
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+
+        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            var text = e?.Message?.Text;
+            if (text == null)
+                return;
+            await _botClient.SendTextMessageAsync(
+                e.Message.Chat,
+                $"Your message:'{text}', /help для получения списка доступных команд").ConfigureAwait(false);
+        }
+    }
+}
